adcfee29c8ce267a8aa02472ba385f621b0f6274
Hunter Tunnicliff <hunter@tunnicliff.co>
2026-09-11 16:11:51 -0700

    Add nock

--- src/__test__/client.test.ts
+++ src/__test__/client.test.ts
@@ -1,18 +1,33 @@
-import { describe, expect, it } from "vitest";
+import nock, { Scope } from "nock";
+import { beforeEach, describe, expect, it } from "vitest";
 
 import { Client, Config } from "../client.ts";
 
+const DEFAULT_SESSION_URL = "https://example.test";
+const DEFAULT_API_URL = `${DEFAULT_SESSION_URL}/special-jmap-api`;
+
 const DEFAULT_OPTIONS = {
   bearerToken: "<tok>",
-  sessionUrl: "https://example.test",
+  sessionUrl: DEFAULT_SESSION_URL,
 } satisfies Config;
 
 describe("Client", () => {
-  it("has expected public properties", () => {
+  let scope: Scope;
+
+  beforeEach(() => {
+    scope = nock(/example\.test/)
+      .get("/")
+      .reply(200, { apiUrl: DEFAULT_API_URL });
+  });
+
+  it("has expected public properties", async () => {
     const client = new Client(DEFAULT_OPTIONS);
     expect(client).toHaveProperty("api");
     expect(client).toHaveProperty("session");
     expect(client).toHaveProperty("refreshSession");
+    const session = await client.session;
+    expect(session.apiUrl).toBe(DEFAULT_API_URL);
+    expect(scope.isDone()).toBe(true);
   });
 
   it("is frozen", () => {
@@ -24,17 +39,16 @@ describe("Client", () => {
     expect(() => new Client({ ...DEFAULT_OPTIONS, sessionUrl: "invalid-url" })).toThrow(
       new Error("Invalid session URL", { cause: "invalid-url" }),
     );
-  });
-
-  it("accepts valid string session URLs", () => {
-    expect(
-      () => new Client({ ...DEFAULT_OPTIONS, sessionUrl: "https://valid.example" }),
-    ).not.toThrow();
+    expect(scope.isDone()).toBe(false);
   });
 
   it("accepts valid URL instance session URLs", () => {
     expect(
-      () => new Client({ ...DEFAULT_OPTIONS, sessionUrl: new URL("https://valid.example") }),
+      () =>
+        new Client({
+          ...DEFAULT_OPTIONS,
+          sessionUrl: new URL(DEFAULT_SESSION_URL),
+        }),
     ).not.toThrow();
   });
 
@@ -42,6 +56,7 @@ describe("Client", () => {
     it("is a pending promise", async () => {
       const client = new Client(DEFAULT_OPTIONS);
       expect(client.session).toBeInstanceOf(Promise);
+      await expect(client.session).resolves.not.toThrow();
     });
   });
 });
