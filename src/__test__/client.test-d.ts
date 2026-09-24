bbd2daec9c74d7bd896b3acd2881edc0664a4d38
Hunter Tunnicliff <hunter@tunnicliff.co>
2026-09-23 20:02:56 -0700

    Update type tests for custom capabilities

--- src/__test__/client.test-d.ts
+++ src/__test__/client.test-d.ts
@@ -3,6 +3,7 @@ import type {
   EmailAddress,
   GetArguments,
   GetResponse,
+  ID,
   Mailbox,
   MailboxRole,
 } from "jmap-rfc-types";
@@ -10,6 +11,9 @@ import { describe, expectTypeOf, it } from "vitest";
 
 import { defineCapability } from "../capability.ts";
 import { Client } from "../client.ts";
+import type { BatchResult } from "../internal/batcher.ts";
+import type { MethodCall } from "../internal/method-calls.ts";
+import type { AllowRefs } from "../ref.ts";
 
 const host = "https://example.test";
 const bearerToken = "<opaque-token>";
@@ -32,12 +36,12 @@ describe("Client", () => {
 
       type Expected = Pick<Email, "id" | "to" | "from">;
 
-      expectTypeOf(email).toExtend<Expected>();
+      expectTypeOf(email).toMatchObjectType<Expected>();
 
       // Present
       expectTypeOf(email.id).toBeString();
-      expectTypeOf(email.to).toEqualTypeOf<EmailAddress[]>();
-      expectTypeOf(email.from).toEqualTypeOf<EmailAddress[]>();
+      expectTypeOf(email.to).toEqualTypeOf<EmailAddress[] | undefined>();
+      expectTypeOf(email.from).toEqualTypeOf<EmailAddress[] | undefined>();
 
       // Not present
       expectTypeOf(email).not.toHaveProperty("keywords");
@@ -49,19 +53,19 @@ describe("Client", () => {
     it("filters entity properties in Mailbox/get requests", async () => {
       const response = await client.api.Mailbox.get({
         ids: ["<pretend-mailbox-id>"],
-        properties: ["id", "name", "role"],
+        properties: ["id", "name", "role", "totalEmails"],
       });
 
       const mailbox = response.list[0]!;
 
-      type Expected = Pick<Mailbox, "id" | "name" | "role">;
+      type Expected = Pick<Mailbox, "id" | "name" | "role" | "totalEmails">;
 
-      expectTypeOf(mailbox).toEqualTypeOf<Expected>();
+      expectTypeOf(mailbox).toMatchObjectType<Expected>();
 
       // Present
       expectTypeOf(mailbox.id).toBeString();
       expectTypeOf(mailbox.name).toBeString();
-      expectTypeOf(mailbox.role).toExtend<MailboxRole | null>();
+      expectTypeOf(mailbox.role).toEqualTypeOf<MailboxRole | null>();
 
       // Not present
       expectTypeOf(mailbox).not.toHaveProperty("totalThreads");
@@ -71,18 +75,19 @@ describe("Client", () => {
     });
 
     describe("when using a custom capability", () => {
-      type Example = {
-        id: string;
-        stuff: string;
-        here: boolean;
-      };
+      interface Example {
+        some: string;
+        things: string[];
+        nested: {
+          properties: [number, number];
+        };
+      }
 
-      type ContractInput = GetArguments<Example>;
-      type ContractOutput<A> = GetResponse<Example, A>;
-
-      interface Contract {
-        input: ContractInput;
-        output: ContractOutput<this["input"]>;
+      type Input = GetArguments<Example>;
+      type Output<A> = GetResponse<Example, A>;
+      interface GetContract {
+        input: Input;
+        output: Output<this["input"]>;
       }
 
       const example = defineCapability({
@@ -90,34 +95,87 @@ describe("Client", () => {
         entities: ["Example"],
       }).withMethods<{
         Example: {
-          get: Contract;
+          get: GetContract;
         };
       }>();
 
-      it("filters entity properties in get requests", async () => {
-        const client = new Client({
-          sessionUrl,
-          bearerToken,
-          capabilities: [example],
+      const client = new Client({
+        sessionUrl,
+        bearerToken,
+        capabilities: [example],
+      });
+
+      it("still has built-ins", () => {
+        expectTypeOf(client.api).toHaveProperty("Email");
+        expectTypeOf(client.api).toHaveProperty("Core");
+        expectTypeOf(client.api).toHaveProperty("Blob");
+        expectTypeOf(client.api).toHaveProperty("VacationResponse");
+      });
+
+      it("respects accountId optionality", () => {
+        type GivenArgs = GetContract["input"];
+        expectTypeOf<GivenArgs["accountId"]>().toEqualTypeOf<ID>();
+
+        type ClientArgs = Parameters<typeof client.api.Example.get>[0];
+        expectTypeOf<ClientArgs["accountId"]>().toEqualTypeOf<AllowRefs<ID | undefined>>();
+      });
+
+      it("respects generics (`properties` for Get requests, in this case)", async () => {
+        const filtered = client.api.Example.get({
+          ids: ["<some-id>"],
+          properties: ["some", "things"],
+        });
+        const unfiltered = client.api.Example.get({
+          ids: ["<some-id>"],
         });
 
-        const response = await client.api.Example.get({
-          ids: ["<pretend-example-id>"],
-          properties: ["stuff", "here"],
+        const [filteredResult, unfilteredResult] = await Promise.all([filtered, unfiltered]);
+
+        // Both are readonly arrays
+        expectTypeOf(filteredResult.list).toExtend<ReadonlyArray<unknown>>();
+        expectTypeOf(unfilteredResult.list).toExtend<ReadonlyArray<unknown>>();
+
+        // Filtered has specified properties
+        expectTypeOf(filteredResult.list.at(0)!).toEqualTypeOf<Pick<Example, "some" | "things">>();
+
+        // Unfiltered has all properties
+        expectTypeOf(unfilteredResult.list.at(0)!).toEqualTypeOf<Example>();
+      });
+
+      it("uses BatchResult types appropriately", async () => {
+        const pending = client.api.Example.get({
+          ids: ["<some-id>"],
         });
 
-        const item = response.list[0]!;
+        // Correct batch result type
+        expectTypeOf(pending).toEqualTypeOf<
+          BatchResult<
+            MethodCall<{ ids: string[] }>,
+            {
+              accountId: ID;
+              state: string;
+              list: readonly Example[];
+              notFound: ReadonlyArray<ID>;
+            }
+          >
+        >();
 
-        type Expected = Pick<Example, "stuff" | "here">;
+        // Correct awaited result
+        const result = await pending;
+        expectTypeOf(result).toMatchObjectType<{
+          accountId: ID;
+          state: string;
+          list: readonly Example[];
+          notFound: ReadonlyArray<ID>;
+        }>();
 
-        expectTypeOf(item).toMatchObjectType<Expected>();
-
-        // Present
-        expectTypeOf(item.stuff).toBeString();
-        expectTypeOf(item.here).toBeBoolean();
-
-        // Not present
-        expectTypeOf(item).not.toHaveProperty("id");
+        // Awaited<T> pending also works
+        expectTypeOf<Awaited<typeof pending>>().toMatchObjectType<{
+          accountId: ID;
+          state: string;
+          list: readonly Example[];
+          notFound: ReadonlyArray<ID>;
+        }>();
       });
     });
   });
